201944a0bc6121d934c4ebc8820a108f5e68bb0b
Hasibullah Mahmood 2 <hasibullahmahmod@gmail.com>
2021-08-04 16:12:53 +0300

    asf

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -4,33 +4,16 @@ import ExpenseItem from '../expenseItem/ExpenseItem';
 import Card from '../card/Card';
 import ExpensesFilter from './ExpensesFilter';
 
-const Expenses = () => {
+const Expenses = ({ expenses }) => {
 	const [year, setYear] = useState('2020');
-	const expenses = [
-		{
-			id: 'e1',
-			title: 'Toilet Paper',
-			amount: 94.12,
-			date: new Date(2020, 7, 14),
-		},
-		{ id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-		{
-			id: 'e3',
-			title: 'Car Insurance',
-			amount: 294.67,
-			date: new Date(2021, 2, 28),
-		},
-		{
-			id: 'e4',
-			title: 'New Desk (Wooden)',
-			amount: 450,
-			date: new Date(2021, 5, 12),
-		},
-	];
+
 	const selectYearHandler = (selectedYear) => {
 		setYear(selectedYear);
-		console.log(selectedYear);
 	};
+
+	expenses = expenses.filter((expense) => {
+		return new Date(expense.date).getFullYear() === +year;
+	});
 	return (
 		<Card className="expenses">
 			<ExpensesFilter onSelectYear={selectYearHandler} selectedYear={year} />
